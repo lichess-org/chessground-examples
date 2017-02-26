1d4ab3c40ec653b4667251672966bf771f73bf00
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-02-26 11:23:58 +0100

    update for chessground v6.0.6

--- src/units/unit.ts
+++ src/units/unit.ts
@@ -4,6 +4,7 @@ import * as basics from './basics'
 import * as play from './play'
 import * as perf from './perf'
 import * as anim from './anim'
+import * as svg from './svg'
 
 export interface Unit {
   name: string;
@@ -14,5 +15,6 @@ export const list: Unit[] = [
   basics.defaults, basics.fromFen,
   play.initial, play.castling, play.vsRandom, play.slowAnim, play.conflictingHold,
   perf.move, perf.select,
-  anim.conflictingAnim
+  anim.conflictingAnim,
+  svg.presetUserShapes, svg.changingShapesHigh, svg.changingShapesLow
 ];
